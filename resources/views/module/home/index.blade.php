9643af0cc05c928a82f4b85a61983fae282a74e4
Abdul <abdulhamid909@gmail.com>
2015-12-15 21:44:01 +0700

    edit index

--- resources/views/module/home/index.blade.php
+++ resources/views/module/home/index.blade.php
@@ -26,15 +26,27 @@
                 <div class="row-fluid">
                   <textarea class="span12" id="article" style="height: 70px; resize: none;"></textarea>
                 </div>
+                <div class="row-fluid" id="imagePost" style="display:none">
+                  <div class="btn-box-row row-fluid">
+                    <a href="#" class="btn-box small span2">
+    									<i class="icon-key"></i>
+    									<b>Small</b>
+    								</a>
+                  </div>
+                </div>
+                <div class="row-fluid">
+                  <input type="text" style="display:none" placeholder="enter your hastag" class="span12" id="hastag" />
+                  <input type="text" style="display:none" placeholder="enter your link" class="span12" id="link" />
+                </div>
                 <div class="clearfix">
                   <button type="button" id="send-btn" class="btn btn-primary pull-right">Updated Status</button>
-                  <a href="#" class="btn btn-small" rel="tooltip" data-placement="top" data-original-title="Upload a photo">
+                  <a href="#" class="imageLink btn btn-small" rel="tooltip" data-placement="top" data-original-title="Upload a photo">
                     <i class="icon-camera shaded"></i>
                   </a>
-                  <a href="#" class="btn btn-small" rel="tooltip" data-placement="top" data-original-title="Upload a video">
+                  <a href="#" class="hastag btn btn-small" rel="tooltip" data-placement="top" data-original-title="Upload a video">
                     <i class="icon-facetime-video shaded"></i>
                   </a>
-                  <a href="#" class="btn btn-small" rel="tooltip" data-placement="top" data-original-title="Pin your location">
+                  <a href="#" class="link btn btn-small" rel="tooltip" data-placement="top" data-original-title="Pin your location">
                     <i class="icon-map-marker shaded"></i>
                   </a>
                 </div>
@@ -323,7 +335,6 @@
                   <div class="stream-attachment video">
                     <div class="responsive-video">
 
-
                           <iframe src="" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="#">Google Car</a> from <a href="#">Henk Rogers</a> on <a href="#">Vimeo</a>.</p>
 
 
@@ -360,23 +371,5 @@
 @stop
 
 @section('script')
-<script type="text/javascript">
-$(document).ready(function(){
-  $('#send-btn').click(function(){
-    console.log("sads");
-    $.ajax({
-      url: 'post/status',
-      // url: {!! url(GLobalHelpers::indexUrl().'/post/status') !!},
-      type: "get",
-      data: {'article':$('#article').val(),
-             'tags':$('input[name=tags]').val(),
-             'link':$('input[name=link]').val(),
-             '_token': $('input[name=_token]').val()},
-      success: function(data){
-        alert(data);
-      }
-    });
-  });
-});
-</script>
+  @include('module.home.js')
 @stop
---
+++ resources/views/module/home/js.blade.php
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+$(document).ready(function(){
+  $('#send-btn').click(function(){
+    console.log("sads");
+    $.ajax({
+      url: 'post/status',
+      // url: {!! url(GLobalHelpers::indexUrl().'/post/status') !!},
+      type: "get",
+      data: {'article':$('#article').val(),
+             'tags':$('input[name=tags]').val(),
+             'link':$('input[name=link]').val(),
+             '_token': $('input[name=_token]').val()},
+      success: function(data){
+        alert(data);
+      }
+    });
+  });
+
+  // Link Action In Post
+  $('a.hastag').click(function (event)
+  {$("#hastag").show();$("#link").hide();$("#imagePost").hide();});
+  $('a.link').click(function (event)
+  {$("#hastag").hide();$("#link").show();$("#imagePost").hide();});
+  $('a.imageLink').click(function (event)
+  { $("#hastag").hide();$("#link").hide(); $("#imagePost").show(); });
+
+});
+</script>
