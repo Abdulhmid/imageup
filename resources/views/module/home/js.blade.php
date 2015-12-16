6b6b2fd38d4d74d93edaabe9e167151a891f8859
Abdul <abdulhamid909@gmail.com>
2015-12-16 19:04:46 +0700

    edit

--- resources/views/module/home/js.blade.php
+++ resources/views/module/home/js.blade.php
@@ -1,17 +1,17 @@
 <!-- The Canvas to Blob plugin is included for image resizing functionality -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
 <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
 <script src="{!! asset('plugins/jQuery-File-Upload-9.11.2/js/vendor/jquery.ui.widget.js') !!}"></script>
 <!-- The Templates plugin is included to render the upload/download listings -->
 <!-- <script src="//blueimp.github.io/JavaScript-Templates/js/tmpl.min.js"></script> -->
 <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-<script src="//blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
+<script src="{!! asset('plugins/jQuery-File-Upload-9.11.2/js/load-image.all.min.js') !!}"></script>
 <!-- The Canvas to Blob plugin is included for image resizing functionality -->
-<script src="//blueimp.github.io/JavaScript-Canvas-to-Blob/js/canvas-to-blob.min.js"></script>
+<script src="{!! asset('plugins/jQuery-File-Upload-9.11.2/js/canvas-to-blob.min.js') !!}"></script>
 <!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!-- <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> -->
 <!-- blueimp Gallery script -->
-<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+<script src="{!! asset('plugins/jQuery-File-Upload-9.11.2/js/jquery.blueimp-gallery.min.js') !!}"></script>
 <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
 <script src="{!! asset('plugins/jQuery-File-Upload-9.11.2/js/jquery.iframe-transport.js') !!}"></script>
 <!-- The basic File Upload plugin -->
--- resources/views/module/home/style.blade.php
+++ resources/views/module/home/style.blade.php
@@ -1,10 +1,9 @@
 
     <!-- jQuery File Uploads  -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <!-- Generic page styles -->
     <link rel="stylesheet" href="{!! asset('plugins/jQuery-File-Upload-9.11.2/css/style.css') !!}">
     <!-- blueimp Gallery styles -->
-    <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+    <link rel="stylesheet" href="{!! asset('plugins/jQuery-File-Upload-9.11.2/css/blueimp-gallery.min.css') !!}">
     <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
     <link rel="stylesheet" href="{!! asset('plugins/jQuery-File-Upload-9.11.2/css/jquery.fileupload.css') !!}">
     <link rel="stylesheet" href="{!! asset('plugins/jQuery-File-Upload-9.11.2/css/jquery.fileupload-ui.css') !!}">
