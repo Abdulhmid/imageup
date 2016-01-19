1a4a55c1d4d363d2cac8bddcbeb31536ab43907a
abdul <abdul.hamid@tlab.co.id>
2016-01-19 23:35:00 +0700

    edit

--- resources/views/module/themes/index.blade.php
+++ resources/views/module/themes/index.blade.php
@@ -13,17 +13,18 @@
 		}
 
 		.first {
-		    background-color: #e3e3e3;
+		    background-color: #FFFDC7;
 		}
 		.second {
-		    background-color: #D286B7;
+		    background-color: #CFDACE;
 		}
 		.col-md-2 {
 		    border-radius: 0 0px 0px 0;
 		}
 		.border{
-		    border-radius: 5px 5px 5px 5px;
+		    border-radius: 11px 11px 11px 11px;
 		    margin-top: 12px;
+		    border: 1px solid #9C8C8C;
 		    background-color: rgba(241, 251, 235, 0.72);
 		}
 		.row > div.mbr-item {
@@ -32,20 +33,25 @@
 		    -moz-outline-radius: 0px;
 		    -webkit-outline-radius: 0px;
 		 }
+		.content {
+			font-size: 12px;
+			font-family: 'Times New Roman', Arial, sans-serif;
+		}
     </style>
 @stop
 
 @section('content')
-<div class="" style="padding-left:18px;padding-right:18px;">
+<div class="content" style="padding-left:18px;padding-right:18px;">
 	<div class="row">
 		@for ($i = 0; $i < 20; $i++)
-		    <div class="col-md-2 col-sm-6 col-xs-12 mbr-item {!! $i % 2 == 0 ? 'first' : 'second' !!}" style="height:173px;{!! ($i == 2 ? 'padding: 0px;' : '' ) !!}">
-		    	@if($i == 2)
-					<img class="img-responsive" src="{!! url('theme/ungkap') !!}/img/member2.jpg" width="224px" style="margin:0 auto;" height="173px" alt="member-2">
-					<!-- <div class="border" >
-			    		<span><i class="fa fa-comment"></i></span>
+		    <div class="col-md-2 col-sm-6 col-xs-12 mbr-item {!! $i % 2 == 0 ? 'first' : 'second' !!}" style="height:173px;{!! ($i == 2 || $i == 15 ? 'padding: 0px;' : '' ) !!}">
+		    	@if($i == 2 || $i == 15)
+					<img class="img-responsive" src="{!! url('theme/ungkap') !!}/img/member2.jpg" width="224px" style="margin:0 auto;height:173px;" alt="member-2">
+					<div class="border" style="margin-top: -35px;">
+			    		<span><i class="fa fa-comment"> 32323</i></span>
+			    		<span><i class="fa fa-users"> 32332</i></span>
 			    		<span><i class="fa fa-users"></i></span>
-			    	</div> -->
+			    	</div>
 		    	@else
 			    	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -54,7 +60,11 @@
 			    		<span><i class="fa fa-comment"> 8989</i></span>
 			    		<span><i class="fa fa-eye"></i> 9090</span>
 			    	</div>
-		    	@endif
+		    	@endif<br/>
+			    <div class="box" style="">
+			    	<button class="btn btn-defaul">Naik</button>
+			    	<button class="btn btn-defaul">Hapus</button>
+			    </div>
 		    </div>
 	    @endfor
 	</div>
